f20d2b110ad02c2446bc0f2f24be6aeef5c1ebd8
Toan <trantuanphuong19941994@gmail.com>
2018-09-28 00:13:36 +0700

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Project from './Components/Project';
 import Header from './Components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   constructor(props) {
@@ -29,7 +30,9 @@ class App extends Component {
     return (
       <div className="App">
         <Header title="My App"/>
-        <Project projects={this.state.projects}/>
+        <div className="container">
+          <Project projects={this.state.projects}/>
+        </div>
       </div>
     );
   }
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 
 class Project extends React.Component {
     render() {
+        const {projects} = this.props;
+        console.log(this.props.projects);
         return (
-            <div>
-                Project
+            <div className="card card-body mb-3">
+                <ul className="list-group">
+                    {projects.map(project =>
+                        <li className="list-group-item">
+                            <h3>{project.category}</h3>
+                            <p>{project.title}</p>
+                        </li>
+                    )}
+                </ul>
             </div>
         );
     }
